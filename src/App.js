0c15b3e55113fff1c6a2c8ab80a9e10a45ceab1d
Jocelyn Yang/WHQ/Wistron <jocelyn_yang@wistron.com>
2023-07-22 19:40:31 +0800

    feat: get repos api to render info

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import { useState, useEffect } from "react";
+import moment from "moment";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [userName, setUserName] = useState("");
+  const [loading, setIsLoading] = useState(false);
+  const [reposData, setReposData] = useState([]);
+
+  const handleChangeUserName = (e) => {
+    const { value } = e.target;
+    setUserName(value);
+  };
+
+  const handleSearch = () => {
+    setIsLoading(true);
+    // Send a GET request
+    axios
+      .get(`https://api.github.com/users/${userName}/repos`)
+      .then((response) => {
+        console.log(response.data);
+        setReposData([...response.data]);
+      })
+      .catch((error) => {
+        console.error("Error:", error.message);
+      });
+  };
+
+  const RepoItems = () => {
+    return reposData.map((repo) => (
+      <div key={repo.id}>
+        <h6>Repo Name</h6>
+        <p>{repo.name}</p>
+        <h6>Link</h6>
+        <a href={repo.url}>{repo.url}</a>
+        <h6>Updated At</h6>
+        <p>{moment(repo.updated_at).format("YYYY-MM-DD")}</p>
+      </div>
+    ));
+  };
+
+  useEffect(() => {
+    setReposData([]);
+  }, [userName]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input
+        value={userName}
+        placeholder='Please input the gitHub username'
+        onChange={(e) => handleChangeUserName(e)}></input>
+      <button onClick={() => handleSearch()}>Search</button>
+      {reposData.length > 0 ? <RepoItems /> : null}
     </div>
   );
 }
